6165551cd1691c30c1a8312380b952de58c19d21
DoriaFabio <doriafabio2001@gmail.com>
2024-11-07 20:38:40 +0100

    change backgroud-color

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const changeText = contButton.innerHTML;
 console.log(changeText);
 const changeImg = document.getElementById("lamp");
 console.log(changeImg);
-
+const views = document.getElementById("body");
 contButton.addEventListener("click", function () {
     console.log(changeImg.src);
     if (contButton.innerText.includes("Accendi")) {
@@ -16,11 +16,13 @@ contButton.addEventListener("click", function () {
         changeImg.alt = "lampOn";
         changeImg.className = "rotateY";
         console.log(changeImg);
+        views.className = "bg-contrast";
     } else if (contButton.innerText.includes("Spegni")) {
         changeImg.src = "./img/white_lamp.png";
         changeImg.alt = "lampOff";
         contButton.innerText = "Accendi";
         changeImg.classList.remove("rotateY");
+        views.classList.remove("bg-contrast");
     }
 }
 );

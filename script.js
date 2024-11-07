440335c3d129c3aa91d7155c063837f168d71ba9
DoriaFabio <doriafabio2001@gmail.com>
2024-11-07 19:57:43 +0100

    do exercise

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 "use strict";
 console.clear();
+
+const contButton = document.getElementById("change");
+console.log(contButton);
+const changeText = contButton.innerHTML;
+console.log(changeText);
+const changeImg = document.getElementById("lamp");
+console.log(changeImg);
+
+contButton.addEventListener("click", function () {
+    console.log(changeImg.src);
+    if (contButton.innerText.includes("Accendi")) {
+        contButton.innerText = "Spegni";
+        changeImg.src = "./img/yellow_lamp.png";
+        changeImg.alt = "lampOn";
+    //    changeImg.src = 
+    } else if (contButton.innerText.includes("Spegni")) {
+        changeImg.src = "./img/white_lamp.png";
+        changeImg.alt = "lampOff";
+        contButton.innerText = "Accendi";
+    }
+}
+);
